0074cecedfa589ad389a18eec465464cac061fcd
Cristhian1938 <cristhiandavila139@gmail.com>
2024-07-07 23:37:39 -0500

    TraspuestaMatriz.c

--- Matriz/TraspuestaMatriz.c
+++ Matriz/TraspuestaMatriz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 //Funcion para hacer la transpuesta
-void transpuesta(int mat[][3], int trans[][3]) {
+void transpuesta(int mat[3][3], int trans[3][3]) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             trans[i][j] = mat[j][i];
@@ -8,7 +8,7 @@ void transpuesta(int mat[][3], int trans[][3]) {
     }
 }
 //Funcion para imprimir la matriz
-void printMatrix(int mat[][3]) {
+void printMatrix(int mat[3][3]) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             printf("%d ", mat[i][j]);
